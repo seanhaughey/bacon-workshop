8bb57510e998eaeac3317478e412123c3f78b4d3
Braden O'Guinn <braden.oguinn@mutualmobile.com>
2016-03-10 11:57:56 -0600

    Adds reduce example
    Signed-off-by: Braden O'Guinn <braden.oguinn@mutualmobile.com>

---
+++ examples/bacon/reduce.js
@@ -0,0 +1,16 @@
+$ = require('baconjs');
+_ = require('lodash');
+
+const countTen = $.sequentially(100, _.times(10));
+
+countTen.scan(0, add)
+.log('scan');
+
+countTen.fold(0, add)
+.log('fold');
+
+countTen.toProperty(0)
+.scan(100, add)
+.log('propertyScan')
+
+function add (a, b) {return a + b}
